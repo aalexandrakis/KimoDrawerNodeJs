46dfa4d6e99d80f2834dc1edb7c1c17dc7af1345
aalexandrakis <aalexandrakis@hotmail.com>
2014-11-18 08:58:54 +0200

    -- lets begin AGAIN -- get next draw date -- set next draw date up to 5 minutes -- create a new draw

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var connection  = require('express-myconnection');
+var mysql = require('mysql');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -22,6 +24,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+
+    connection(mysql,{
+
+        host: 'localhost',
+        user: 'kimo',
+        password : 'kimo',
+        port : 3306, //port mysql
+        database:'kimo'
+    },'request')
+);//route index, hello world
+
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/drawer.js
+++ routes/drawer.js
@@ -2,7 +2,7 @@ var express = require('express');
 var Random = require('random-js');
 var router = express.Router();
 var Q = require('q');
-var connection;
+
 
 // create a Mersenne Twister-19937 that is auto-seeded based on time and other random values
 var engine = Random.engines.mt19937().autoSeed();
@@ -11,20 +11,76 @@ var distribution = Random.integer(1, 80);
 // generate a number that is guaranteed to be within [0, 99] without any particular bias.
 
 router.get('/', function(req, res) {
-    drawNumbers = [];
+
+    function errorFunction(err){
+        error = new Error();
+        error.status = 500;
+        error.message = err;
+        return error;
+    }
+    //get next draw
+    function getNextDraw(){
+        df = new Q.defer();
+        req.getConnection(function(err,connection){
+            if(err){
+                df.reject(errorFunction(err));
+            } else {
+                query = "SELECT nextDraw FROM next_draw ";
+                connection.query(query ,function(err,nextDraw)     {
+                    if(err){
+                        df.reject(errorFunction(err));
+                    } else {
+                        df.resolve(nextDraw[0].nextDraw);
+                    }
+                });
+            }
+        });
+        return df.promise;
+    }
+
+    //save next draw + 5 minutes
+    function saveNextDraw(currentDraw){
+        df = new Q.defer();
+        req.getConnection(function(err,connection){
+            if(err){
+                df.reject(errorFunction(err));
+            } else {
+                nextDraw = new Date(currentDraw.getTime() + (5 * 60000));
+                nextDrawString = nextDraw.getFullYear().toString() + "-" +
+                                 (nextDraw.getMonth() + 1 < 10 ? ("0" + nextDraw.getMonth() + 1) : (nextDraw.getMonth() + 1)).toString() + "-"+
+                                 (nextDraw.getDate() < 10 ? "0" + nextDraw.getDate() : nextDraw.getDate()).toString() + " " +
+                                 (nextDraw.getHours() < 10 ? "0" + nextDraw.getHours() : nextDraw.getHours()).toString() + ":" +
+                                 (nextDraw.getMinutes() < 10 ? "0" + nextDraw.getMinutes() : nextDraw.getMinutes()).toString() + ":" +
+                                 (nextDraw.getSeconds() < 10 ? "0" + nextDraw.getSeconds() : nextDraw.getSeconds()).toString();
+
+                query = "UPDATE next_draw set nextDraw = '" + nextDrawString + "'";
+                connection.query(query, function(err,nextDrawRow)     {
+                    if(err){
+                        df.reject(errorFunction(err));
+                    } else {
+                        df.resolve({'currentDraw':currentDraw, 'nextDraw':nextDraw});
+                    }
+                });
+            }
+        });
+        return df.promise;
+    }
     //Create 20 random numbers
-    function drawFunction(){
-       result = [];
+    function drawFunction(input){
+       numbers = [];
        df = new Q.defer();
-       while (result.length != 20){
-            console.log("setTimeOut ", result);
-            setInterval(function(){
+       while (numbers.length != 20){
                number = distribution(engine);
-               if (result.indexOf(number) == -1){
-                  result.push(number);
+               if (numbers.indexOf(number) == -1){
+                  numbers.push(number);
                }
-            } , 1);
-            if (result.length == 20){
+            if (numbers.length == 20){
+                 numbers = numbers.sort(function(a,b){
+                    return a - b;
+                 });
+                 result = {};
+                 result.drawDateTime = new Date();
+                 result.drawNumbers = numbers;
                  df.resolve(result);
             }
        }
@@ -32,9 +88,12 @@ router.get('/', function(req, res) {
    }
 
     Q().then(function(result){
+            return getNextDraw();
+     }).then(function(result){
+            return saveNextDraw(result);
+     }).then(function(result){
             return drawFunction();
      }).then(function(result){
-        clearInterval(getRandomNumber);
         res.status(200).send(result);
     }).catch(function(result){
         res.status(500).send(error);
