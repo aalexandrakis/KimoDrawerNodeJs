6567d8d08f55dfd7b6c46d9a760e8ce4330ec556
aalexandrakis <aalexandrakis@hotmail.com>
2014-11-26 23:40:16 +0200

    -- revert server.js -- add a dummy start script on package.json

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("LET'S BEGIN");
+#!/usr/bin/env node
+var debug = require('debug')('kimodrawer');
+var app = require('./app');
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3005);
+
+var server = app.listen(app.get('port'), function() {
+//  debug('Express server listening on port ' + server.address().port);
+});
