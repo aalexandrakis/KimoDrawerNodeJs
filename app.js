615a7e1e1a284026bd066e31d0bc665eee30e2fd
aalexandrakis <aalexandrakis@hotmail.com>
2014-11-27 00:34:09 +0200

    -- add mysql ip

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
     connection(mysql,{
-        host: 'localhost',
+        host: '127.3.55.130:3306',
         user: 'kimo',
         password : 'kimo',
         port : 3306, //port mysql
