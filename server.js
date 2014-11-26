ebc8e689f8781cff60e30aa4b77f07dbd3ea41fb
aalexandrakis <aalexandrakis@hotmail.com>
2014-11-26 23:36:25 +0200

    -- change server.js to print only a log

--- server.js
+++ server.js
@@ -1,9 +1,1 @@
-#!/usr/bin/env node
-var debug = require('debug')('kimodrawer');
-var app = require('./app');
-
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3005);
-
-var server = app.listen(app.get('port'), function() {
-//  debug('Express server listening on port ' + server.address().port);
-});
+console.log("LET'S BEGIN");
