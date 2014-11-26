eb55c36cbd4a333270250323e2ad46c70798f01a
aalexandrakis <aalexandrakis@hotmail.com>
2014-11-26 23:50:15 +0200

    -- revert all previous changes -- add a main item in package.json

--- bin/www.js
+++ bin/www.js
@@ -2,8 +2,10 @@
 var debug = require('debug')('kimodrawer');
 var app = require('../app');
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3005);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3005
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-var server = app.listen(app.get('port'), function() {
-//  debug('Express server listening on port ' + server.address().port);
+
+var server = app.listen(server_port, server_ip_address, function() {
+  debug('Express server listening on port ' + server.address().port);
 });
