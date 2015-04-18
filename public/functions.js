3ca7faac2dc2e969e220f3cce4d31f27a1b2ef24
Alexandrakis Alexandros <aalexandrakis@hotmail.com>
2015-04-18 18:47:09 +0300

    -- remove add 1 in months

--- public/functions.js
+++ public/functions.js
@@ -2,7 +2,7 @@ module.exports = {
 
 convertDateToMySqlTimeStampString : function(timestamp){
                                          return timestamp.getFullYear().toString() + "-" +
-                                         (timestamp.getMonth() + 1 < 10 ? ("0" + timestamp.getMonth() + 1) : (timestamp.getMonth() + 1)).toString() + "-"+
+                                         (timestamp.getMonth() < 10 ? ("0" + timestamp.getMonth()) : (timestamp.getMonth())).toString() + "-"+
                                          (timestamp.getDate() < 10 ? "0" + timestamp.getDate() : timestamp.getDate()).toString() + " " +
                                          (timestamp.getHours() < 10 ? "0" + timestamp.getHours() : timestamp.getHours()).toString() + ":" +
                                          (timestamp.getMinutes() < 10 ? "0" + timestamp.getMinutes() : timestamp.getMinutes()).toString() + ":" +
@@ -11,7 +11,7 @@ convertDateToMySqlTimeStampString : function(timestamp){
 
 convertDateToIsoString : function(timestamp){
                                          return (timestamp.getDate() < 10 ? "0" + timestamp.getDate() : timestamp.getDate()).toString() +
-                                         (timestamp.getMonth() + 1 < 10 ? ("0" + timestamp.getMonth() + 1) : (timestamp.getMonth() + 1)).toString() +
+                                         (timestamp.getMonth() < 10 ? ("0" + timestamp.getMonth() ) : (timestamp.getMonth() )).toString() +
                                          timestamp.getFullYear().toString() +
                                          (timestamp.getHours() < 10 ? "0" + timestamp.getHours() : timestamp.getHours()).toString() +
                                          (timestamp.getMinutes() < 10 ? "0" + timestamp.getMinutes() : timestamp.getMinutes()).toString()+
@@ -23,10 +23,6 @@ addDelimitersToIsoDate : function(timestamp){
                                 timestamp.substring(8,10)+":"+timestamp.substring(10,12)+":"+timestamp.substring(12,14);
 },
 
-addOneMonth : function(timestamp){
-             d = new Date(timestamp)
-             return d.setMonth(d.getMonth + 1);
-},
 
 fromIsoToEuro: function(dateString){
 		var regExp = /(\d{4}).(\d{2}).(\d{2}).(\d{2}).(\d{2}).(\d{2}).*/;
